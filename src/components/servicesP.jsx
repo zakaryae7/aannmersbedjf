d27a97113310a04cc74a73214d764a58860f9af6
RiffiHamza <hamza.riffi@outlook.fr>
2023-09-03 17:32:00 +0100

    make  basic styling

--- src/components/servicesP.jsx
+++ src/components/servicesP.jsx
@@ -1,35 +1,36 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import './style.css'; // Replace 'styles.css' with the path to your CSS file.
 
 export function ServicesP(props) {
-    return (
-        <div>
-            <div className="lg:m-20 mx-5 mt-20">
-                <h1 className="text-orange-400 text-3xl font-semibold">
-                    <span>{props.id}-</span> {props.title}
-                </h1>
-                <div className='flex lg:flex-row flex-col justify-around items-center'>
-                    <img src={props.src} className='shadow-ezze my-5 lg:w-[500px] w-80' />
-                    <div className='lg:w-1/3 text-center lg:text-left justify-center lg:justify-normal flex flex-col gap-5'>
-                        <p>{props.description}</p>
-                        <Link to={props.btn} className=' bg-orange-400 hover:bg-orange-600 lg:w-max duration-300 px-2 py-1.5 rounded text-white font-medium'>
-                            Offerte aanvraag
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            {props.services && props.services.length > 0 && ( // Check if props.services exists and has items
-                <div className="mt-5">
-                    <h2 className="text-xl font-semibold">Mini-Services:</h2>
-                    <ul>
-                        {props.services.map((miniservice, index) => (
-                            <li key={index}>{miniservice}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+  return (
+    <div>
+      <div className="lg:m-20 mx-5 mt-20">
+        <h1 className="text-orange-400 text-3xl font-semibold">
+          <span>{props.id}-</span> {props.title}
+        </h1>
+        <div className='flex lg:flex-row flex-col justify-around items-center'>
+          <img src={props.src} className='shadow-ezze my-5 lg:w-[500px] w-80' />
+          <div className='lg:w-1/3 text-center lg:text-left justify-center lg:justify-normal flex flex-col gap-5'>
+            <p>{props.description}</p>
+            <Link to={props.btn} className=' bg-orange-400 hover:bg-orange-600 lg:w-max duration-300 px-2 py-1.5 rounded text-white font-medium'>
+              Offerte aanvraag
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+      {props.services && props.services.length > 0 && (
+        <div className="mt-5">
+          <h2 className="text-xl font-semibold">Mini-Services:</h2>
+          <ul className="mini-services-list">
+            {props.services.map((miniservice, index) => (
+              <li key={index} className="mini-service-item">{miniservice}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default ServicesP;
